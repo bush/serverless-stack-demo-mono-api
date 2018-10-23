69831b8305091ffd443854e0e8e34972e41bc259
Dave Bush <davebush@gmail.com>
2018-10-23 19:17:22 -0400

    Removed authorizer
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- services/notes/get-dev.sh
+++ services/notes/get-dev.sh
@@ -3,11 +3,11 @@
 apig-test \
 --username='admin@example.com' \
 --password='Passw0rd!' \
---user-pool-id='us-east-1_N7jxTF7DV' \
---app-client-id='5i166nq8omk64sa43c6lkaviq7' \
+--user-pool-id='us-east-1_XLZw1Yfs1' \
+--app-client-id='3sms6r6qnujhdnb10ita4tp4sl' \
 --cognito-region='us-east-1' \
---identity-pool-id='us-east-1:cb4e7e98-7438-4c13-a379-c281ebaceefc' \
---invoke-url='https://vgxt38owr1.execute-api.us-east-1.amazonaws.com/prod' \
+--identity-pool-id='us-east-1:ba14299e-dd40-4e51-962e-39dd0cdef961' \
+--invoke-url='https://5x3wjhrhlg.execute-api.us-east-1.amazonaws.com/dev' \
 --api-gateway-region='us-east-1' \
 --path-template='/notes' \
 --method='GET' \
