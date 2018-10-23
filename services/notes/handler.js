bdccb3d7286f7ef889bf01fcdd2a03f482bec27f
Dave Bush <davebush@gmail.com>
2018-10-23 07:00:43 -0400

    Another change ...
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- services/notes/handler.js
+++ services/notes/handler.js
@@ -4,7 +4,7 @@ module.exports.main = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully! v5' + process.env.tableName,
+      message: 'Go Serverless v1.0! Your function executed successfully! v6: ' + process.env.tableName,
       input: event,
     }),
   };
